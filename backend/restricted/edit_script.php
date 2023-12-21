750c03c80c47c698ac734ca8602e773682600999
Gabriel-Santos1 <gabrielpaulino538@gmail.com>
2023-12-21 19:53:38 -0300

    Feat:backend implementation

---
+++ backend/restricted/edit_script.php
@@ -0,0 +1,60 @@
+<?php
+include "../validation.php";
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alteração de cadastrado</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <?php
+            include "connection.php";
+            $id = $_POST["id"];
+            $name = $_POST['name'];
+            $cpf = $_POST['cpf'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $phone = $_POST['phone'];
+
+            //$sql = "INSERT INTO `people`(`name`, `cpf`, `email`, `password`, `phone`) 
+
+                //VALUES ('$name','$cpf','$email','$password','$phone')";
+
+            $sql = "UPDATE `people` set `name` = '$name', `cpf` = '$cpf', `email` = '$email', 
+            `password`= '$password' , `phone`='$phone' WHERE cod_people = $id";
+
+
+            if (mysqli_query($conn, $sql)) {
+                mensagem ("$name Alterado com sucesso", 'success');
+            } else {
+                mensagem ("$name Não foi possível alterar", 'danger');
+
+            }
+
+            ?>
+
+            <a href = "search.php"><button type="button" class="btn btn-outline-danger">Voltar</button></a>
+        </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
