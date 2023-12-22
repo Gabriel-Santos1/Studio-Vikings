44dd8c973936dc3382204dca3d833af2a19cf994
Gabriel-Santos1 <gabrielpaulino538@gmail.com>
2023-12-22 17:53:07 -0300

    feat:Logout and Validation.

--- backend/validation/validation.php
+++ backend/validation/validation.php
@@ -5,18 +5,5 @@
         unset($_SESSION['email']);
         unset($_SESSION['password']);
     }
-    
-    
+  
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>System</title>
-</head>
-<body>
-    
-</body>
-</html>
